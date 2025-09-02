ae8653a28daf4ade5c150d819fdea20e7890d2ee
Sigsign <sig@signote.cc>
2025-09-03 07:06:43 +0900

    Remove starter template

--- src/entrypoints/popup/main.tsx
+++ src/entrypoints/popup/main.tsx
@@ -1,6 +1,5 @@
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
 
-import './style.css';
-import App from './App';
+import App from "./App";
 
-render(() => <App />, document.getElementById('root')!);
+render(() => <App />, document.getElementById("root")!);
