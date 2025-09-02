d230b46f617858cfc795dd41b12de96905f7c258
Sigsign <sig@signote.cc>
2025-09-03 08:32:52 +0900

    Set extension name

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "wxt";
 
 export default defineConfig({
+  manifest: {
+    name: "YouTube Auto Toggle Live Chat",
+  },
   modules: ["@wxt-dev/module-solid"],
   srcDir: "src",
 });
