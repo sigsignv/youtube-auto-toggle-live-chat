8768a2c3307fd46f1a54f22bc7e253e971952c21
Sigsign <sig@signote.cc>
2025-09-05 21:20:27 +0900

    Update Popup UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,27 +1,58 @@
 import { createSignal, onMount } from "solid-js";
-import { liveChatCollapsed } from "@/utils/storage";
+import { liveChatCollapsed, liveChatReplayExpanded } from "@/utils/storage";
 
 import "./App.css";
 
 function App() {
   const [isLiveChatCollapsed, setIsLiveChatCollapsed] = createSignal(false);
+  const [isLiveChatReplayExpanded, setIsLiveChatReplayExpanded] =
+    createSignal(false);
 
   onMount(async () => {
-    const value = await liveChatCollapsed.getValue();
-    setIsLiveChatCollapsed(value);
+    const liveChatCollapsedPromise = async () => {
+      const value = await liveChatCollapsed.getValue();
+      setIsLiveChatCollapsed(value);
+    };
+    const liveChatReplayExpandedPromise = async () => {
+      const value = await liveChatReplayExpanded.getValue();
+      setIsLiveChatReplayExpanded(value);
+    };
+
+    await Promise.allSettled([
+      liveChatCollapsedPromise(),
+      liveChatReplayExpandedPromise(),
+    ]);
   });
 
   return (
-    <section>
-      <h2>Live Chat</h2>
-      <input
-        type="checkbox"
-        checked={isLiveChatCollapsed()}
-        onChange={async (e) => {
-          await liveChatCollapsed.setValue(e.target.checked);
-        }}
-      />
-    </section>
+    <form>
+      <fieldset>
+        <legend>Live streaming</legend>
+        <label>
+          Live chat auto-expanded :
+          <input
+            type="checkbox"
+            checked={!isLiveChatCollapsed()}
+            onChange={async (e) => {
+              await liveChatCollapsed.setValue(!e.target.checked);
+            }}
+          />
+        </label>
+      </fieldset>
+      <fieldset>
+        <legend>Live archive / Premieres</legend>
+        <label>
+          Live chat replay auto-expanded :
+          <input
+            type="checkbox"
+            checked={isLiveChatReplayExpanded()}
+            onChange={async (e) => {
+              await liveChatReplayExpanded.setValue(e.target.checked);
+            }}
+          />
+        </label>
+      </fieldset>
+    </form>
   );
 }
 
