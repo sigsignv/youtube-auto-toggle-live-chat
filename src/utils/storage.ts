746d9edf06b32e3c66b02d70fb735e44a8940833
Sigsign <sig@signote.cc>
2025-09-11 13:00:18 +0900

    Make chat collapsed by default after install

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -3,7 +3,7 @@ import { storage } from "#imports";
 export const liveChatCollapsed = storage.defineItem<boolean>(
   "local:liveChatCollapsed",
   {
-    fallback: false,
+    fallback: true,
   },
 );
 
